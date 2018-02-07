2540b075b4d8c664cfac18304cd3d554316b0a29
Jomuss <36137125+Jomuss@users.noreply.github.com>
2018-02-07 16:45:04 -0500

    initial repo

---
+++ FinalApplication/src/finalapplication/MorseSerialManager.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalapplication;
+import processing.serial.*;
+import processing.core.PApplet;
+
+
+/**
+ *
+ * @author Joe Moss
+ */
+public class MorseSerialManager{
+    static Serial myPort;
+    
+    public MorseSerialManager(PApplet inst){
+        myPort = new Serial(inst, Serial.list()[0], 9600);
+    }
+    public void setup(){
+    }
+    public void settings(){
+ 
+      
+    }
+}
