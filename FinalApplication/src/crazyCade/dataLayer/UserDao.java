6d161a0bd2c0325e5f84ea674de7850567019885
Alexander Groenewold <36244995+agroenewold28@users.noreply.github.com>
2018-03-04 16:22:28 -0400

    Add files via upload

--- FinalApplication/src/crazyCade/dataLayer/UserDao.java
+++ FinalApplication/src/crazyCade/dataLayer/UserDao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package crazyCade.dataLayer;
+package datalayer;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -22,19 +22,18 @@ import java.util.ArrayList;
 public class UserDao {
     public static ArrayList<String> userComponents;
     
-    public static void addUser(UserModel user) throws IOException{
+    public static void addUser(PlayerModel user) throws IOException{
         FileWriter fw = null;
         BufferedWriter bw = null;
-        File userFile = new File("C:/Users/Alex/Java/StartingWindows/src/gamedao/" + user.userName + ".txt");
+        File userFile = new File("C:/Users/Alex/Java/StartingWindows/src/gamedao/" + user.getUserName() + ".txt");
         userFile.createNewFile();        
         try {
             fw = new FileWriter(userFile);
             bw = new BufferedWriter(fw);
-            bw.write(user.userName + "\n");
-            bw.write(user.password + "\n");
-            bw.write(String.valueOf(user.overallScore + "\n"));
-            bw.write(String.valueOf(user.pongScore + "\n"));
-            bw.write(String.valueOf(user.checkerScore));
+            bw.write(user.getUserName() + "\n");
+            bw.write(user.getPassword() + "\n");
+            bw.write(user.getPongRecord() + "\n");
+            bw.write(user.getCheckersRecord());
 //            FileOutputStream fos;
 //            fos = new FileOutputStream(file);
 //            ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -51,11 +50,14 @@ public class UserDao {
         }               
     }
     
-    public static UserModel getUser(File file){
+    public static PlayerModel getUser(File file){
         userComponents = new ArrayList();
-        UserModel user = null;
+        PlayerModel user = null;
         BufferedReader reader = null;
         String line;
+        int[] pongRecordArray = new int[2];
+        int[] checkersRecordArray = new int[2];
+        int i = 0;
         try {
             if(!file.exists())
                 return null;
@@ -63,10 +65,25 @@ public class UserDao {
                 reader = new BufferedReader(new FileReader(file));
                 line = reader.readLine();
                 while(line != null){
+                    if(i > 1){
+                        if(i == 2){
+                            pongRecordArray[0] = line.charAt(0);
+                            pongRecordArray[1] = line.charAt(2);
+                        }
+                        else{
+                            checkersRecordArray[0] = line.charAt(0);
+                            checkersRecordArray[1] = line.charAt(2);
+                        }
+                    }
                     userComponents.add(line);
                     line = reader.readLine();
+                    i++;
                 }
-                user = new UserModel(userComponents.get(0), userComponents.get(1), Integer.valueOf(userComponents.get(2)), Integer.valueOf(userComponents.get(3)), Integer.valueOf(userComponents.get(4)));
+                user = new PlayerModel();
+                user.setUserName(userComponents.get(0));
+                user.setPassword(userComponents.get(1));
+                user.setPongRecord(pongRecordArray);
+                user.setCheckersRecord(checkersRecordArray);
             }
         } catch(IOException e){
             e.printStackTrace();
