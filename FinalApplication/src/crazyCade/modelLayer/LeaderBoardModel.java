90a4b210c92368c35cba623716a0c8961b0ff8ef
Alexander Groenewold <36244995+agroenewold28@users.noreply.github.com>
2018-04-02 16:28:10 -0300

    Add files via upload

--- FinalApplication/src/crazyCade/modelLayer/LeaderBoardModel.java
+++ FinalApplication/src/crazyCade/modelLayer/LeaderBoardModel.java
@@ -5,6 +5,8 @@
  */
 package crazyCade.modelLayer;
 
+import crazyCade.UI.MainWindow;
+import crazyCade.dataLayer.UserDao;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,18 +25,16 @@ public class LeaderBoardModel {
         leaderBoard = new ArrayList();
     }
     
-    public void populateLeaderboardFromPlayerData(){
-        ArrayList<String> userComponents = new ArrayList();
-        File usersFolder = new File("C:/Users/Alex/Desktop/CrazyCade-origin (2)/CrazyCade-origin(real)/FinalApplication/src/crazyCade/gameDao");  
-        PlayerModel p;
+    public void populateLeaderboardFromPlayerData(int leaderboardType){
+        ArrayList<String> userComponents = new ArrayList();  
+        PlayerModel player;
         String line;
         int i = 0;
         int j = 0;
         int size = 0;
-        File[] folder = usersFolder.listFiles();
-        for(File userFile : folder){
+        for(int k = 1; k <= UserDao.userCount; k++){
             try{
-                BufferedReader reader = new BufferedReader(new FileReader(userFile));
+                BufferedReader reader = new BufferedReader(new FileReader(new File("C:/Users/Alex/Desktop/CrazyCade-origin (2)/CrazyCade-origin(real)/FinalApplication/src/crazyCade/gameDao/user" + k + ".txt")));
                 line = reader.readLine();
                 while(line != null){
                     if(i > 1){
@@ -52,13 +52,16 @@ public class LeaderBoardModel {
             } catch(IOException e){
                 System.out.print("IO Exception caught");
             }
-            p = new PlayerModel(userComponents.get(0), userComponents.get(1), 
+            player = new PlayerModel(userComponents.get(0), userComponents.get(1), 
             Integer.valueOf(userComponents.get(2)), Integer.valueOf(userComponents.get(3)),
             Integer.valueOf(userComponents.get(4)), Integer.valueOf(userComponents.get(5)));
-            leaderBoard.add(j, p);
+            if(userComponents.get(0).equals(MainWindow.getCurUsername()))
+                player.declareCurPlayer();
+            leaderBoard.add(j, player);
             j++;
+            userComponents = new ArrayList();
         }
-        this.sortPlayers();
+        this.sortPlayers(leaderboardType);
     }
     
     public PlayerModel getPlayer(int place){
@@ -67,21 +70,27 @@ public class LeaderBoardModel {
         return player;
     }
     
-    private void sortPlayers(){
+    private void sortPlayers(int leaderboardType){
         ArrayList<PlayerModel> temp = new ArrayList();
         int placer = leaderBoard.size();
         double bestScore = 0;
         for(int j = 0; j < leaderBoard.size(); j++){
+            if(leaderboardType == 0)
+                leaderBoard.get(j).setCurWinPct(leaderboardType);
+            else if(leaderboardType == 1)
+                leaderBoard.get(j).setCurWinPct(leaderboardType);
+            else
+                leaderBoard.get(j).setCurWinPct(leaderboardType);
             leaderBoard.get(j).setPosition(j);
         }
         for(int i = 0; i < leaderBoard.size(); i++){
             for(int k = 0; k < placer; k++){
-                if(leaderBoard.get(i).getWinPct() > bestScore){
-                    bestScore = leaderBoard.get(i).getWinPct();
-                }
+                if(leaderBoard.get(i).getCurWinPct()> bestScore){
+                    bestScore = leaderBoard.get(i).getCurWinPct();
+                }               
             }
             for(PlayerModel p : leaderBoard){
-                if(p.getWinPct() == bestScore)
+                if(p.getCurWinPct() == bestScore)
                     p.setPosition(i);               
             }
             bestScore = 0;
