69b501ff3b6332f60b2d62f31e8e8fba045d6dde
Alexander Groenewold <36244995+agroenewold28@users.noreply.github.com>
2018-04-22 23:51:48 -0300

    Add files via upload

--- FinalApplication/src/crazyCade/modelLayer/LeaderBoardModel.java
+++ FinalApplication/src/crazyCade/modelLayer/LeaderBoardModel.java
@@ -5,6 +5,7 @@
  */
 package crazyCade.modelLayer;
 
+import crazyCade.ManagerLayer.PlayerManager;
 import crazyCade.UI.MainWindow;
 import crazyCade.dataLayer.UserDao;
 import java.io.BufferedReader;
@@ -12,7 +13,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.ArrayList;  
 
 /**
  *
@@ -25,24 +26,19 @@ public class LeaderBoardModel {
         leaderBoard = new ArrayList();
     }
     
-    public void populateLeaderboardFromPlayerData(int leaderboardType){
+    public final void populateLeaderboardFromPlayerData(int leaderboardType){
         ArrayList<String> userComponents = new ArrayList();  
         PlayerModel player;
         String line;
         int i = 0;
         int j = 0;
         int size = 0;
-        for(int k = 0; k <= UserDao.userCount; k++){
+        for(int k = 0; k < UserDao.userCount; k++){
             try{
-                BufferedReader reader = new BufferedReader(new FileReader(new File("C:/Users/Joe Moss/Desktop/CrazyCade/FinalApplication/src/gameDao/user" + k + ".txt")));
+                BufferedReader reader = new BufferedReader(new FileReader(new File("C:/Users/Alex/Desktop/CrazyCade-origin (2)/CrazyCade-origin(real)/FinalApplication/src/crazyCade/gameDao/user" + k + ".txt")));
                 line = reader.readLine();
                 while(line != null){
-                    if(i > 1){
-                        userComponents.add(String.valueOf(line.charAt(0)));    
-                        userComponents.add(String.valueOf(line.charAt(4)));
-                    }
-                    else
-                        userComponents.add(line);
+                    userComponents.add(line);
                     line = reader.readLine();
                     i++;
                 }
@@ -52,58 +48,79 @@ public class LeaderBoardModel {
             } catch(IOException e){
                 System.out.print("IO Exception caught");
             }
-            player = new PlayerModel(userComponents.get(0), userComponents.get(1), 
-            Integer.valueOf(userComponents.get(2)), Integer.valueOf(userComponents.get(3)),
-            Integer.valueOf(userComponents.get(4)), Integer.valueOf(userComponents.get(5)));
+            player = new PlayerModel(userComponents.get(0), userComponents.get(1), userComponents.get(2),
+            Integer.valueOf(userComponents.get(3)), Integer.valueOf(userComponents.get(4)),
+            Integer.valueOf(userComponents.get(5)), Integer.valueOf(userComponents.get(6)));
             if(userComponents.get(0).equals(MainWindow.getCurUsername()))
                 player.declareCurPlayer();
             leaderBoard.add(j, player);
             j++;
             userComponents = new ArrayList();
         }
-        this.sortPlayers(leaderboardType);
+        this.sortPlayers(leaderboardType, false);
     }
     
-    public PlayerModel getPlayer(int place){
-        PlayerModel player = new PlayerModel();
-        player = leaderBoard.get(place--);
-        return player;
+    public void updateLeaderboard(int leaderBoardType, PlayerModel curPlayer){
+        for(int i = 0; i < leaderBoard.size(); i++){
+            if(curPlayer.getUserName().equals(leaderBoard.get(i).getUserName())){
+                leaderBoard.remove(i);
+                leaderBoard.add(i, curPlayer);
+            }
+        }
+        this.sortPlayers(leaderBoardType, true);
     }
     
-    private void sortPlayers(int leaderboardType){
+//    public PlayerModel getPlayer(int place){
+//        PlayerModel player = new PlayerModel();
+//        player = leaderBoard.get(place--);
+//        return player;
+//    }
+    
+    private void sortPlayers(int leaderboardType, Boolean firstSort){
+        int x = 0;
+        int y = 0;
         ArrayList<PlayerModel> temp = new ArrayList();
         int placer = leaderBoard.size();
-        double bestScore = 0;
+        double bestScore = 0.0;
         for(int j = 0; j < leaderBoard.size(); j++){
             if(leaderboardType == 0)
-                leaderBoard.get(j).setCurWinPct(leaderboardType);
+                leaderBoard.get(j).setCurWinPct(leaderBoard.get(j).getOvrWinPct());
             else if(leaderboardType == 1)
-                leaderBoard.get(j).setCurWinPct(leaderboardType);
+                leaderBoard.get(j).setCurWinPct(leaderBoard.get(j).getPongWinPct());
             else
-                leaderBoard.get(j).setCurWinPct(leaderboardType);
-            leaderBoard.get(j).setPosition(j);
+                leaderBoard.get(j).setCurWinPct(leaderBoard.get(j).getCheckersWinPct());
+            leaderBoard.get(j).setTempPosition(j);
         }
-        for(int i = 0; i < leaderBoard.size(); i++){
-            for(int k = 0; k < placer; k++){
-                if(leaderBoard.get(i).getCurWinPct()> bestScore){
-                    bestScore = leaderBoard.get(i).getCurWinPct();
+        for(int i = 0; i < placer; i++){
+            for(int k = 0; k < leaderBoard.size(); k++){
+                if(leaderBoard.get(k).getCurWinPct() > bestScore){
+                    bestScore = leaderBoard.get(k).getCurWinPct();
                 }               
             }
-            for(PlayerModel p : leaderBoard){
-                if(p.getCurWinPct() == bestScore)
-                    p.setPosition(i);               
+            if(bestScore == 0.0){
+                for(int n = 0; n < leaderBoard.size(); n++){
+                    temp.add(leaderBoard.get(n));
+                }
+                break;
             }
-            bestScore = 0;
-        }
-        for(int l = 0; l < leaderBoard.size(); l++){
-            for(PlayerModel p : leaderBoard){
-                if(p.getPosition() == l)
-                    temp.add(p);
+            else{
+                for(int m = 0; m < leaderBoard.size(); m++){
+                    if(leaderBoard.get(m).getCurWinPct() == bestScore){
+                        temp.add(leaderBoard.get(m));
+                        leaderBoard.remove(leaderBoard.get(m));
+                    }
+                }
             }
+            bestScore = 0.0;
         }
+        
         leaderBoard = new ArrayList();
-        for(PlayerModel p : temp){
-            leaderBoard.add(p);
+        leaderBoard.addAll(temp);
+        
+        for(int i = 0; i < leaderBoard.size(); i++){
+            leaderBoard.get(i).setPosition(i);
+            if(firstSort)
+                this.checkPlayerPositionChanged(leaderBoard.get(i), leaderboardType);
         }
     }
     
@@ -113,5 +130,21 @@ public class LeaderBoardModel {
     
     public void addToLeaderboard(PlayerModel p){
         this.leaderBoard.add(p);
+    }
+    
+    private void checkPlayerPositionChanged(PlayerModel changingPlayer, int leaderBoardType){
+        String stringType; 
+        if(leaderBoardType == 0)
+            stringType = "Overall";
+        else if(leaderBoardType == 1)
+            stringType = "Pong";
+        else
+            stringType = "Checkers";
+        if(changingPlayer.getTempPosition() != changingPlayer.getPosition()){
+            if(changingPlayer.getPosition() > changingPlayer.getTempPosition())
+                PlayerManager.sendPlayerEmailForPositionChange(changingPlayer, false, stringType);           
+            else
+                PlayerManager.sendPlayerEmailForPositionChange(changingPlayer, true, stringType);
+        }
     }
 }
--- FinalApplication/src/crazyCade/modelLayer/PlayerModel.java
+++ FinalApplication/src/crazyCade/modelLayer/PlayerModel.java
@@ -14,30 +14,39 @@ package crazyCade.modelLayer;
 public class PlayerModel {
     private String userName;
     private String password;  
+    private final String email;
     private int pongWins;
-    private int[] pongRecord;
+    private int pongLosses;
     private int checkersWins;
-    private int[] checkersRecord;
+    private int checkersLosses;
     private int overallWins;
-    private int position;
+    private int position, tempPosition;
     private Boolean curPlayer = false;
+    private double ovrWinPct;
+    private double pongWinPct;
+    private double checkersWinPct;
     private double curWinPct;
     
-    public PlayerModel(){
-        this.setArrays();
-    }
+//    public PlayerModel(){
+//
+//    }
     
-    public PlayerModel(String userName, String password, int pongWins, int pongLosses, int checkersWins, int checkersLosses){
-        this.setArrays();
+      public PlayerModel(String userName, String password, String email, int pongWins, int pongLosses, int checkersWins, int checkersLosses){
         this.userName = userName;
         this.password = password;
-        this.pongRecord[0] = pongWins;
-        this.checkersRecord[0] = checkersWins;
-        this.pongRecord[1] = pongLosses;
-        this.checkersRecord[1] = checkersLosses;
-        this.pongWins = this.pongRecord[0];
-        this.checkersWins = this.checkersRecord[0];
-        this.overallWins = this.checkersWins + this.pongWins;
+        this.email = email;
+        this.pongWins = pongWins;
+        this.checkersWins = checkersWins;
+        this.pongLosses = pongLosses;
+        this.checkersLosses = checkersLosses;
+        this.updatePlayer();
+    }
+      
+    private Boolean checkGamesPlayed(int leaderboardType){
+        if(this.calculateGamesPlayed(leaderboardType) == 0)
+            return false;
+        else 
+            return true;
     }
 
     /**
@@ -72,58 +81,60 @@ public class PlayerModel {
      * @return the pongWins
      */
     public int getPongWins() {
-        return this.pongRecord[0];
+        return this.pongWins;
     }
 
     /**
      * @param pongWins the pongWins to set
      */
-    public void addToPongWins(int pongWins) {
-        this.pongWins = this.pongWins + pongWins;
-        this.pongRecord[0] = this.pongWins;
+    public void addToPongWins() {
+        this.pongWins++;
+        this.updatePlayer();
     }
 
     /**
      * @return the pongRecord
      */
     public String getPongRecord() {
-        return pongRecord[0]+" - "+pongRecord[1];
+        return String.valueOf(pongWins)+" - "+ String.valueOf(pongLosses);
     }
 
     /**
      * @param pongRecord the pongRecord to set
      */
-    public void setPongRecord(int[] pongRecord) {
-        this.pongRecord = pongRecord;
+    public void setPongRecord(int wins, int losses) {
+        this.pongWins = wins;
+        this.pongLosses = losses;
     }
 
     /**
      * @return the checkersWins
      */
     public int getCheckersWins() {
-        return this.checkersRecord[0];
+        return this.checkersWins;
     }
 
     /**
      * @param checkersWins the checkersWins to set
      */
-    public void addToCheckersWins(int checkersWins) {
-        this.checkersWins = this.checkersWins + checkersWins;
-        this.checkersRecord[0] = this.checkersWins;
+    public void addToCheckersWins() {
+        this.checkersWins++;
+        this.updatePlayer();
     }
 
     /**
      * @return the checkersRecord
      */
     public String getCheckersRecord() {
-        return checkersRecord[0]+" - "+checkersRecord[1];
+        return checkersWins+" - "+checkersLosses;
     }
 
     /**
      * @param checkersRecord the checkersRecord to set
      */
-    public void setCheckersRecord(int[] checkersRecord) {
-        this.checkersRecord = checkersRecord;
+    public void setCheckersRecord(int wins, int losses) {
+        this.checkersWins = wins;
+        this.checkersLosses = losses;
     }
 
     /**
@@ -140,24 +151,17 @@ public class PlayerModel {
         this.overallWins = overallWins;
     }
     
-    public void setCurWinPct(int leaderboardType){
-        int gamesPlayed = pongRecord[0] + pongRecord[1] + checkersRecord[0] + checkersRecord[1];
-        int checkersGamesPlayed = checkersRecord[0] + checkersRecord[1];
-        int pongGamesPlayed = pongRecord[0] + pongRecord[1];
-        if(overallWins == 0)
-            curWinPct = 0;
-        else{
-            if(leaderboardType == 0)
-                curWinPct = gamesPlayed / overallWins;
-            else if(leaderboardType == 1)
-                curWinPct = pongGamesPlayed / pongRecord[0];
-            else 
-                curWinPct = checkersGamesPlayed / checkersRecord[0];
-        }
+    private int calculateGamesPlayed(int leaderboardType){
+        if(leaderboardType == 0)
+            return pongWins + pongLosses + checkersWins + checkersLosses;
+        else if(leaderboardType == 1)
+            return pongWins + pongLosses;
+        else
+            return checkersWins + checkersLosses;
     }
     
-    public double getCurWinPct(){
-        return this.curWinPct;
+    public int getTotalGamesPlayed(){
+        return this.calculateGamesPlayed(0);
     }
     
     public int getPosition(){
@@ -166,11 +170,6 @@ public class PlayerModel {
     
     public void setPosition(int position){
         this.position = position;
-    }
-    
-    private void setArrays(){    
-        pongRecord = new int[2];
-        checkersRecord = new int[2];
     }
     
     public void declareCurPlayer(){
@@ -183,8 +182,109 @@ public class PlayerModel {
     
     public void playerLost(Boolean pong){
         if(pong)
-            this.pongRecord[1]++;
+            this.pongLosses++;
         else
-            this.checkersRecord[1]++;
+            this.checkersLosses++;
+        this.updatePlayer();
+    }
+    
+    public int getPongLosses(){
+        return this.pongLosses;
+    }
+    
+    public int getCheckersLosses(){
+        return this.checkersLosses;
+    }
+    
+    public double getOvrWinPct(){
+        return this.ovrWinPct;
+    }
+    
+    public double getPongWinPct(){
+        return this.pongWinPct;
+    }
+    
+    public double getCheckersWinPct(){
+        return this.checkersWinPct;
+    }
+    
+    public void setCurWinPct(double winPct){
+        this.curWinPct = winPct;
+    }
+    
+    public double getCurWinPct(){
+        return this.curWinPct;
+    }
+    
+    private double roundCurWinPct(double curWinPct){
+        curWinPct = Math.round(curWinPct * 100.0) / 100.0;
+        return curWinPct;
+    }
+    
+    private void updatePlayer(){
+        this.overallWins = this.checkersWins + this.pongWins;
+        try{
+            if(this.checkGamesPlayed(0)){
+                this.ovrWinPct = ((double)this.overallWins / (double)this.calculateGamesPlayed(0));
+                this.ovrWinPct = this.roundCurWinPct(ovrWinPct);
+            }
+            else
+                this.ovrWinPct = 0.0;
+        } catch(ArithmeticException a){
+            this.ovrWinPct = 0.0;
+        }
+        try{
+            if(this.checkGamesPlayed(1)){
+                this.pongWinPct = (double)this.pongWins / (double)this.calculateGamesPlayed(1);
+                this.pongWinPct = this.roundCurWinPct(pongWinPct);
+            }
+            else 
+                this.pongWinPct = 0.0;
+        } catch(ArithmeticException a){
+            this.pongWinPct = 0.0;
+        }
+        try{
+            if(this.checkGamesPlayed(2)){
+                this.checkersWinPct = (double)this.checkersWins / (double)this.calculateGamesPlayed(2);
+                this.checkersWinPct = this.roundCurWinPct(checkersWinPct);
+            }
+            else 
+                this.checkersWinPct = 0.0;
+        } catch(ArithmeticException a){
+            this.checkersWinPct = 0.0;
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setTempPosition(int tempPosition){
+        this.tempPosition = tempPosition;
+    }
+    
+    public int getTempPosition(){
+        return this.tempPosition;
+    }
+    
+    public String convertToPositionString(){
+        int temp = this.getPosition() + 1;
+        if(this.getLastInt(position) == 1)
+            return String.valueOf(temp) + "st";
+        else if(this.getLastInt(position) == 2)
+            return String.valueOf(temp) + "nd";
+        else if(this.getLastInt(position) == 3)
+            return String.valueOf(temp) + "rd";
+        else
+            return String.valueOf(temp) + "th";
+    }
+    
+    private int getLastInt(int position){
+        String numString = Integer.toString(position);
+        int[] nums = new int[numString.length()];
+        for(int i = 0; i < numString.length(); i++){
+            nums[i] = numString.charAt(i);
+        }
+        return nums[numString.length() - 1];
     }
 }
